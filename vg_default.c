7c21045f697fc83b7b4ba28d9a587375f94a42b5
njn <njn@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2002-08-07 20:32:38 +0000

    Added vg_default.c which I forgot to add with the big commit.
    Also added vg_eraser.c which... I guess never got put in, even though this is
    the ERASER branch.  Duh.
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/ERASER@568 a5019735-40e9-0310-863c-91ae7b9d1cf9

---
+++ vg_default.c
@@ -0,0 +1,281 @@
+/*--------------------------------------------------------------------*/
+/*--- Default panicky definitions of template functions that skins ---*/
+/*--- should override.                                             ---*/
+/*---                                                vg_defaults.c ---*/
+/*--------------------------------------------------------------------*/
+
+/*
+   This file is part of Valgrind, an x86 protected-mode emulator 
+   designed for debugging and profiling binaries on x86-Unixes.
+
+   Copyright (C) 2000-2002 Nicholas Nethercote
+      njn25@cam.ac.uk
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307, USA.
+
+   The GNU General Public License is contained in the file LICENSE.
+*/
+
+
+/* These functions aren't intended to be run.  Replacement functions used by
+ * the chosen skin are substituted by compiling the skin into a .so and
+ * LD_PRELOADing it.  Nasty :) */
+
+#include "vg_include.h"
+
+/* ---------------------------------------------------------------------
+   Fundamental template functions
+   ------------------------------------------------------------------ */
+
+/* If the skin fails to define one or more of the required functions,
+ * make it very clear what went wrong! */
+
+static char* fund_panic =
+    "\nExtension error:\n"
+    "  The skin you have selected is missing one or more of the\n"
+    "  required fundamental functions.  Please check it and try again.\n";
+
+void SK_(init)(void)
+{
+   VG_(printf)(fund_panic);
+   VG_(panic)("called SK_(init)");
+}
+
+UCodeBlock* SK_(instrument) ( UCodeBlock* cb, Addr not_used )
+{
+   VG_(printf)(fund_panic);
+   VG_(panic)("called SK_(instrument)");
+}
+
+void SK_(fini)(void)
+{
+   VG_(printf)(fund_panic);
+   VG_(panic)("called SK_(fini)");
+}
+
+void SK_(setup)(VgNeeds* needs)
+{
+   VG_(printf)(fund_panic);
+   VG_(panic)("called SK_(setup)");
+}
+
+/* ---------------------------------------------------------------------
+   For throwing out basic block level info when code is invalidated
+   ------------------------------------------------------------------ */
+
+void SK_(discard_basic_block_info)(TTEntry* tte)
+{
+   VG_(printf)(fund_panic);
+   VG_(panic)("called SK_(discard_basic_block_info)");
+}
+
+/* ---------------------------------------------------------------------
+   Command line arg template function
+   ------------------------------------------------------------------ */
+
+static char* nonfund_panic =
+    "\nExtension error:\n"
+    "  The skin you have selected is missing one or more of the\n"
+    "  functions required by the skin's needs.  Please check it and\n"
+    "  try again.\n";
+
+void SKN_(process_cmd_line_options)(UInt argc, UChar* argv[])
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(process_cmd_line_options)");
+}
+
+/* ---------------------------------------------------------------------
+   Client request template function
+   ------------------------------------------------------------------ */
+
+UInt SKN_(handle_client_request) ( ThreadState* tst, UInt* arg_block )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(handle_client_request)");
+}
+
+/* ---------------------------------------------------------------------
+   UInstr augmentation
+   ------------------------------------------------------------------ */
+
+void SKN_(augmentUInstr)(UInstr* u)
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(augmentUInstr)");
+}
+
+/* ---------------------------------------------------------------------
+   UCode extension
+   ------------------------------------------------------------------ */
+
+void SKN_(emitExtUInstr)(UInstr* u)
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(emitExtUInstr)");
+}
+
+Bool SKN_(saneExtUInstr)(Bool beforeRA, UInstr* u)
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(saneExtUInstr)");
+}
+
+Char* SKN_(nameExtUOpcode)(Opcode opc)
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(nameExtUOpcode)");
+}
+
+void SKN_(ppExtUInstr)(UInstr* u)
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(ppExtUInstr)");
+}
+
+Int SKN_(getExtTempUsage)(UInstr* u, TempUse* arr)
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(getExtTempUsage)");
+}
+
+/* ---------------------------------------------------------------------
+   Shadow memory
+   ------------------------------------------------------------------ */
+
+void SKN_(make_segment_noaccess) ( Addr a, UInt len )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(make_segment_noaccess)");
+}
+
+void SKN_(make_segment_readable) ( Addr a, UInt len )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(make_segment_readable)");
+}
+
+void SKN_(make_noaccess) ( Addr a, UInt len )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(make_noaccess)");
+}
+
+void SKN_(make_writable) ( Addr a, UInt len )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(make_writable)");
+}
+
+void SKN_(make_readable) ( Addr a, UInt len )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(make_readable)");
+}
+
+void SKN_(make_readwritable) ( Addr a, UInt len )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(make_readwritable)");
+}
+
+void SKN_(copy_address_range_state) ( Addr src, Addr dst, UInt len )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(copy_address_range_state)");
+}
+
+
+
+Bool SKN_(check_writable) ( Addr a, UInt len, Addr* bad_addr )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(check_writable)");
+}
+
+// JJJ: a special one:  can be called (from vg_execontext.c) by any function
+// that asks for an execution context, even if shadow memory isn't needed.
+// So give a default definition (which may be bogus).
+Bool SKN_(check_readable) ( Addr a, UInt len, Addr* bad_addr )
+{
+   return True;
+}
+
+Bool SKN_(check_readable_asciiz) ( Addr a, Addr* bad_addr )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(check_readable_asciiz)");
+}
+
+
+void SKN_(make_aligned_word_WRITABLE) ( Addr a )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(make_aligned_word_WRITABLE)");
+}
+
+
+void SKN_(make_aligned_word_NOACCESS) ( Addr a )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(make_aligned_word_WRITABLE)");
+}
+
+
+void SKN_(init_shadow_memory)(void)
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(init_shadow_memory)");
+}
+
+Bool SKN_(first_and_last_secondaries_look_plausible) ( void )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(first_and_last_secondaries_look_plausible)");
+}
+
+void SKN_(expensive_shadow_memory_sanity_check) ( void )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(expensive_shadow_memory_sanity_check)");
+}
+
+
+/* ---------------------------------------------------------------------
+   Thread tracking
+   ------------------------------------------------------------------ */
+
+void SKN_(thread_does_lock)  ( ThreadId tid, void* mutex )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(thread_does_lock)");
+}
+
+void SKN_(thread_does_unlock)  ( ThreadId tid, void* mutex )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(thread_does_unlock)");
+}
+
+void SKN_(thread_dies) ( void )
+{
+   VG_(printf)(nonfund_panic);
+   VG_(panic)("called SKN_(thread_dies)");
+}
+
+/*--------------------------------------------------------------------*/
+/*--- end                                            vg_defaults.c ---*/
+/*--------------------------------------------------------------------*/
