dbe730130b1d5f0c45d0b793d7888ffe8fe78dd2
njn <njn@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2002-09-11 17:07:31 +0000

    Changed all the SKN_ prefixes to SK_ -- distinction was pointless.
    Changed all the VGM_ prefixes to VG_ -- old distinction which used to make
    sense doesn't really any more.
    
    Changed all the public functions/variables in memcheck skins with VG_ prefixes
    to have SK_ prefixes, to make it clear what comes from core and what comes from
    skin.
    
    Changed all the VgT_foo tag opcodes to Tag_foo for same reason.
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/unlabeled-1.9.4@935 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- coregrind/vg_transtab.c
+++ coregrind/vg_transtab.c
@@ -498,7 +498,7 @@ void VG_(invalidate_translations) ( Addr start, UInt range )
          continue;
 
       if (VG_(needs).identifies_basic_blocks)
-         SKN_(discard_basic_block_info)( vg_tt[i].orig_addr, 
+         SK_(discard_basic_block_info)( vg_tt[i].orig_addr, 
                                          vg_tt[i].orig_size );
 
       vg_tt[i].orig_addr = VG_TTE_DELETED;
