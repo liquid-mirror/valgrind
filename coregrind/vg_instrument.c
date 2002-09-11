785557a3c64e8795b0be96bf5e2be7de822ad703
njn <njn@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2002-09-11 13:04:19 +0000

    Merged in this change from VALGRIND_1_0_X branch:
    \begin{quote}
    1.  The license is actually in the file COPYING, not LICENSE as all
        the sources claim.  Automake seems to have some hard-wired notion
        that the license file must be called COPYING, so we have to
        rename in all the source files :-(
    
    2.  Change the license for valgrind.h ONLY to a BSD-style license
        so people can include it in their code.  The entire rest of
        the system remains under the GPL.
    \end{quote}
    
    
    
    Also:
    
    - made vg_memcheck.h have a BSD-style license too, for the same reason.
    
    - added a license notice to valgrind.in, which was missing one
      (likewise should probably be done for 1_0_X branch;  also there's no
      COPYING.LIB file which two demangler files mention)
    
    - added a license notice to vg_memcheck_include.h, which was missing one.
    
    - wibble in vg_annotate.in
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/unlabeled-1.1.2@928 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- coregrind/vg_instrument.c
+++ coregrind/vg_instrument.c
@@ -25,7 +25,7 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307, USA.
 
-   The GNU General Public License is contained in the file LICENSE.
+   The GNU General Public License is contained in the file COPYING.
 */
 
 // SSS: should this file should eventually not be in core, but included in
