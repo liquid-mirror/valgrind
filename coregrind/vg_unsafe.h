d08b4765d6bfac38bbd634cdb169aaf07b84ce54
jseward <jseward@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2003-11-04 22:29:34 +0000

    Back out revs    coregrind/vg_unsafe.h     1.42.2.2    coregrind/vg_intercept.c  1.18.2.2 We don't want dependencies on kernel headers, esp in the stable branch. Leave it in head for now; see if anybody yelps there.
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/VALGRIND_2_0_REALLY@2002 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- coregrind/vg_unsafe.h
+++ coregrind/vg_unsafe.h
@@ -46,9 +46,6 @@
 #include <net/if.h>       /* for struct ifreq et al */
 #include <net/if_arp.h>   /* for struct arpreq */
 #include <net/route.h>    /* for struct rtentry */
-#ifdef KERNEL_2_6
-#include <linux/compiler.h>
-#endif
 #include <asm/ipc.h>      /* for struct ipc_kludge */
 #include <linux/msg.h>    /* for struct msgbuf */
 #include <linux/sem.h>    /* for struct sembuf */
