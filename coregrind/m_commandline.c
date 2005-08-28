cb79ce6b63f2ae59d29ba7cf5539980184c2533f
tom <tom@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2005-08-28 18:54:17 +0000

    Abstract out the command line handling from m_main.c and make the launcher use it to process the command line when looking for the tool to use.
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/ASPACEM@4553 a5019735-40e9-0310-863c-91ae7b9d1cf9

---
+++ coregrind/m_commandline.c
@@ -0,0 +1,253 @@
+
+/*--------------------------------------------------------------------*/
+/*--- Command line handling.                       m_commandline.c ---*/
+/*--------------------------------------------------------------------*/
+
+/*
+   This file is part of Valgrind, a dynamic binary instrumentation
+   framework.
+
+   Copyright (C) 2000-2005 Julian Seward 
+      jseward@acm.org
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307, USA.
+
+   The GNU General Public License is contained in the file COPYING.
+*/
+
+#include "pub_core_basics.h"
+#include "pub_core_commandline.h"
+#include "pub_core_libcassert.h"
+#include "pub_core_libcbase.h"
+#include "pub_core_libcfile.h"
+#include "pub_core_libcprint.h"
+#include "pub_core_libcproc.h"
+#include "pub_core_mallocfree.h"
+#include "pub_core_options.h"
+
+// Note that we deliberately don't free the malloc'd memory.  See comment
+// at call site.
+static char* get_file_clo(char* dir)
+{
+   Int    n;
+   SysRes fd;
+   struct vki_stat s1;
+   Char* f_clo = NULL;
+   Char  filename[VKI_PATH_MAX];
+
+   VG_(snprintf)(filename, VKI_PATH_MAX, "%s/.valgrindrc", 
+                           ( NULL == dir ? "" : dir ) );
+   fd = VG_(open)(filename, 0, VKI_S_IRUSR);
+   if ( !fd.isError ) {
+      if ( 0 == VG_(fstat)(fd.val, &s1) ) {
+         f_clo = VG_(malloc)(s1.st_size+1);
+         vg_assert(f_clo);
+         n = VG_(read)(fd.val, f_clo, s1.st_size);
+         if (n == -1) n = 0;
+         f_clo[n] = '\0';
+      }
+      VG_(close)(fd.val);
+   }
+   return f_clo;
+}
+
+static Int count_args(char* s)
+{
+   Int n = 0;
+   if (s) {
+      char* cp = s;
+      while (True) {
+         // We have alternating sequences: blanks, non-blanks, blanks...
+         // count the non-blanks sequences.
+         while ( VG_(isspace)(*cp) )    cp++;
+         if    ( !*cp )                 break;
+         n++;
+         while ( !VG_(isspace)(*cp) && *cp ) cp++;
+      }
+   }
+   return n;
+}
+
+// Add args out of environment, skipping multiple spaces and "--" args.
+// We split 's' into multiple strings by replacing whitespace with nuls,
+// eg. "--aa --bb --cc" --> "--aa\0--bb\0--cc".  And for each new string
+// carved out of 's', we put a pointer to it in 'to'.
+static char** copy_args( char* s, char** to )
+{
+   if (s) {
+      char* cp = s;
+      while (True) {
+         // We have alternating sequences: blanks, non-blanks, blanks...
+         // copy the non-blanks sequences, and add terminating '\0'
+         while ( VG_(isspace)(*cp) )    cp++;
+         if    ( !*cp )                 break;
+         *to++ = cp;
+         while ( !VG_(isspace)(*cp) && *cp ) cp++;
+         if ( *cp ) *cp++ = '\0';            // terminate if not the last
+         if (VG_STREQ(to[-1], "--")) to--;   // undo any '--' arg
+      }
+   }
+   return to;
+}
+
+// Augment command line with arguments from environment and .valgrindrc
+// files.
+static void augment_command_line(Int* vg_argc_inout, char*** vg_argv_inout)
+{
+   int    vg_argc0 = *vg_argc_inout;
+   char** vg_argv0 = *vg_argv_inout;
+
+   // get_file_clo() allocates the return value with malloc().  We do not
+   // free f1_clo and f2_clo as they get put into vg_argv[] which must persist.
+   char*  env_clo = VG_(getenv)(VALGRINDOPTS);
+   char*  f1_clo  = get_file_clo( VG_(getenv)("HOME") );
+   char*  f2_clo  = get_file_clo(".");
+
+   /* copy any extra args from file or environment, if present */
+   if ( (env_clo && *env_clo) || (f1_clo && *f1_clo) || (f2_clo && *f2_clo) ) {
+      /* ' ' separated extra options */
+      char **from;
+      char **to;
+      int orig_arg_count, env_arg_count, f1_arg_count, f2_arg_count;
+
+      for ( orig_arg_count = 0; vg_argv0[orig_arg_count]; orig_arg_count++ );
+
+      env_arg_count = count_args(env_clo);
+      f1_arg_count  = count_args(f1_clo);
+      f2_arg_count  = count_args(f2_clo);
+
+      if (0)
+	 VG_(printf)("extra-argc=%d %d %d\n",
+                     env_arg_count, f1_arg_count, f2_arg_count);
+
+      /* +2: +1 for null-termination, +1 for added '--' */
+      from     = vg_argv0;
+      vg_argv0 = VG_(malloc)( (orig_arg_count + env_arg_count + f1_arg_count 
+                              + f2_arg_count + 2) * sizeof(char **));
+      vg_assert(vg_argv0);
+      to      = vg_argv0;
+
+      /* copy argv[0] */
+      *to++ = *from++;
+
+      /* Copy extra args from env var and file, in the order: ~/.valgrindrc,
+       * $VALGRIND_OPTS, ./.valgrindrc -- more local options are put later
+       * to override less local ones. */
+      to = copy_args(f1_clo,  to);
+      to = copy_args(env_clo, to);
+      to = copy_args(f2_clo,  to);
+    
+      /* copy original arguments, stopping at command or -- */
+      while (*from) {
+	 if (**from != '-')
+	    break;
+	 if (VG_STREQ(*from, "--")) {
+	    from++;		/* skip -- */
+	    break;
+	 }
+	 *to++ = *from++;
+      }
+
+      /* add -- */
+      *to++ = "--";
+
+      vg_argc0 = to - vg_argv0;
+
+      /* copy rest of original command line, then NULL */
+      while (*from) *to++ = *from++;
+      *to = NULL;
+   }
+
+   *vg_argc_inout = vg_argc0;
+   *vg_argv_inout = vg_argv0;
+}
+
+void VG_(get_command_line)( int argc, char** argv,
+                            Int* vg_argc_out, Char*** vg_argv_out, 
+                                              char*** cl_argv_out )
+{
+   int    vg_argc0;
+   char** vg_argv0;
+   char** cl_argv;
+   char*  env_clo = VG_(getenv)(VALGRINDCLO);
+
+   if (env_clo != NULL && *env_clo != '\0') {
+      char *cp;
+      char **cpp;
+
+      /* OK, VALGRINDCLO is set, which means we must be a child of another
+         Valgrind process using --trace-children, so we're getting all our
+         arguments from VALGRINDCLO, and the entire command line belongs to
+         the client (including argv[0]) */
+      vg_argc0 = 1;		/* argv[0] */
+      for (cp = env_clo; *cp; cp++)
+	 if (*cp == VG_CLO_SEP)
+	    vg_argc0++;
+
+      vg_argv0 = VG_(malloc)(sizeof(char **) * (vg_argc0 + 1));
+      vg_assert(vg_argv0);
+
+      cpp = vg_argv0;
+
+      *cpp++ = "valgrind";	/* nominal argv[0] */
+      *cpp++ = env_clo;
+
+      // Replace the VG_CLO_SEP args separator with '\0'
+      for (cp = env_clo; *cp; cp++) {
+	 if (*cp == VG_CLO_SEP) {
+	    *cp++ = '\0';	/* chop it up in place */
+	    *cpp++ = cp;
+	 }
+      }
+      *cpp = NULL;
+      cl_argv = argv;
+
+   } else {
+      Bool noaugment = False;
+
+      /* Count the arguments on the command line. */
+      vg_argv0 = argv;
+
+      for (vg_argc0 = 1; vg_argc0 < argc; vg_argc0++) {
+         Char* arg = argv[vg_argc0];
+         if (arg[0] != '-') /* exe name */
+	    break;
+	 if (VG_STREQ(arg, "--")) { /* dummy arg */
+	    vg_argc0++;
+	    break;
+	 }
+         VG_BOOL_CLO(arg, "--command-line-only", noaugment)
+      }
+      cl_argv = &argv[vg_argc0];
+
+      /* Get extra args from VALGRIND_OPTS and .valgrindrc files.
+         Note we don't do this if getting args from VALGRINDCLO, as 
+         those extra args will already be present in VALGRINDCLO.
+         (We also don't do it when --command-line-only=yes.) */
+      if (!noaugment)
+	 augment_command_line(&vg_argc0, &vg_argv0);
+   }
+
+   if (0) {
+      Int i;
+      for (i = 0; i < vg_argc0; i++)
+         VG_(printf)("vg_argv0[%d]=\"%s\"\n", i, vg_argv0[i]);
+   }
+
+   *vg_argc_out =         vg_argc0;
+   *vg_argv_out = (Char**)vg_argv0;
+   *cl_argv_out =         cl_argv;
+}
--- coregrind/m_launcher.c
+++ coregrind/m_launcher.c
@@ -28,6 +28,7 @@
    The GNU General Public License is contained in the file COPYING.
 */
 
+#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,17 +36,26 @@
 #include <unistd.h>
 
 #include "pub_core_basics.h"
+#include "pub_core_commandline.h"
 #include "pub_core_debuglog.h"
+#include "pub_core_libcassert.h"
+#include "pub_core_libcbase.h"
+#include "pub_core_libcfile.h"
+#include "pub_core_libcprint.h"
 #include "pub_core_libcproc.h"   // For VALGRINDLIB
+#include "pub_core_mallocfree.h"
 
 /* Where we expect to find all our aux files */
 static const char *valgrind_lib = VG_LIBDIR;
 
 int main(int argc, char** argv, char** envp)
 {
+   int i, loglevel;
+   Int vg_argc;
+   Char **vg_argv;
+   char **cl_argv;
    const char *toolname = NULL;
    const char *cp;
-   int i, loglevel;
    char *toolfile;
 
    /* Start the debugging-log system ASAP.  First find out how many 
@@ -58,14 +68,22 @@ int main(int argc, char** argv, char** envp)
         break;
      if (0 == strcmp(argv[i], "-d")) 
         loglevel++;
-     if (0 == strncmp(argv[i], "--tool=", 7)) 
-        toolname = argv[i] + 7;
    }
 
    /* ... and start the debug logger.  Now we can safely emit logging
       messages all through startup. */
    VG_(debugLog_startup)(loglevel, "Stage 1");
 
+   /* Get the full command line */
+   VG_(get_command_line)(argc, argv, &vg_argc, &vg_argv, &cl_argv);
+
+   /* Look for a --tool switch */
+   for (i = 1; i < vg_argc; i++) {
+     if (0 == strncmp(vg_argv[i], "--tool=", 7)) 
+        toolname = vg_argv[i] + 7;
+   }
+
+   /* Make sure we know which tool we're using */
    if (toolname) {
       VG_(debugLog)(1, "stage1", "tool %s requested\n", toolname);
    } else {
@@ -88,4 +106,103 @@ int main(int argc, char** argv, char** envp)
    fprintf(stderr, "valgrind: failed to start %s: %s", toolname, strerror(errno));
 
    exit(1);
+}
+
+void* VG_(malloc) ( SizeT nbytes )
+{
+   return malloc(nbytes);
+}
+
+Bool VG_(isspace) ( Char c )
+{
+   return isspace(c);
+}
+
+Int VG_(strcmp) ( const Char* s1, const Char* s2 )
+{
+   return strcmp(s1, s2);
+}
+
+Char *VG_(getenv)(Char *varname)
+{
+   return getenv(varname);
+}
+
+SysRes VG_(open) ( const Char* pathname, Int flags, Int mode )
+{
+   SysRes res;
+   Int fd;
+   if ((fd = open(pathname, flags, mode)) < 0) {
+      res.isError = True;
+      res.val = errno;
+   } else {
+      res.isError = False;
+      res.val = fd;
+   }
+   return res;
+}
+
+void VG_(close) ( Int fd )
+{
+   close(fd);
+   return;
+}
+
+Int VG_(read) ( Int fd, void* buf, Int count)
+{
+   return read(fd, buf, count);
+}
+
+Int VG_(fstat) ( Int fd, struct vki_stat* buf )
+{
+   return fstat(fd, buf);
+}
+
+UInt VG_(snprintf) ( Char* buf, Int size, const HChar *format, ... )
+{
+   va_list vargs;
+   UInt n;
+
+   va_start(vargs, format);
+   n = vsnprintf(buf, size, format, vargs);
+   va_end(vargs);
+
+   return n;
+}
+
+void VG_(assert_fail) ( Bool isCore, const Char* expr, const Char* file, 
+                        Int line, const Char* fn, const HChar* format, ... )
+{
+   va_list vargs;
+   Char buf[256];
+
+   va_start(vargs, format);
+   vsprintf(buf, format, vargs);
+   va_end(vargs);
+
+   fprintf(stderr, "\nvalgrind: %s:%d (%s): Assertion '%s' failed.\n",
+           file, line, fn, expr );
+   if (!strcmp(buf, ""))
+      fprintf(stderr, "valgrind: %s\n", buf);
+
+   exit(1);
+}
+
+static Bool isterm ( Char c )
+{
+   return ( VG_(isspace)(c) || 0 == c );
+}
+
+Int VG_(strcmp_ws) ( const Char* s1, const Char* s2 )
+{
+   while (True) {
+      if (isterm(*s1) && isterm(*s2)) return 0;
+      if (isterm(*s1)) return -1;
+      if (isterm(*s2)) return 1;
+
+      if (*(UChar*)s1 < *(UChar*)s2) return -1;
+      if (*(UChar*)s1 > *(UChar*)s2) return 1;
+
+      s1++; s2++;
+   }
 }
