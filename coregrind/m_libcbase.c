6168cacfb7f60974e797dae503daf0e5a08abc6e
sewardj <sewardj@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2005-09-25 10:16:46 +0000

    Unroll loop in VG_(memcpy).  This is a bit less effective than unroll-and-vectorise, but it's also simpler.
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/ASPACEM@4759 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- coregrind/m_libcbase.c
+++ coregrind/m_libcbase.c
@@ -337,6 +337,20 @@ void* VG_(memcpy) ( void *dest, const void *src, SizeT sz )
    const Char *s = (const Char *)src;
    Char *d = (Char *)dest;
 
+   while (sz >= 8) {
+      d[0] = s[0];
+      d[1] = s[1];
+      d[2] = s[2];
+      d[3] = s[3];
+      d[4] = s[4];
+      d[5] = s[5];
+      d[6] = s[6];
+      d[7] = s[7];
+      sz -= 8;
+      d += 8;
+      s += 8;
+   }
+
    while (sz--)
       *d++ = *s++;
 
