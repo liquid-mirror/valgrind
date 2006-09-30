a2e0544be6db9abb15445f49adbda5c9c56530ea
sewardj <sewardj@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2006-09-30 10:39:37 +0000

    Minor naming / const-ness changes.
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/AIX5@6096 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- coregrind/m_coredump/coredump-elf.c
+++ coregrind/m_coredump/coredump-elf.c
@@ -29,6 +29,7 @@
 */
 
 #include "pub_core_basics.h"
+#include "pub_core_vki.h"
 #include "pub_core_aspacemgr.h"
 #include "pub_core_libcbase.h"
 #include "pub_core_machine.h"
@@ -289,7 +290,7 @@ void make_elf_coredump(ThreadId tid, const vki_siginfo_t *si, UInt max_size)
    Char *coreext = "";
    Int seq = 0;
    Int core_fd;
-   NSegment *seg;
+   NSegment const * seg;
    ESZ(Ehdr) ehdr;
    ESZ(Phdr) *phdrs;
    Int num_phdrs;
@@ -322,11 +323,11 @@ void make_elf_coredump(ThreadId tid, const vki_siginfo_t *si, UInt max_size)
                        VKI_O_CREAT|VKI_O_WRONLY|VKI_O_EXCL|VKI_O_TRUNC, 
                        VKI_S_IRUSR|VKI_S_IWUSR);
       if (!sres.isError) {
-         core_fd = sres.val;
+         core_fd = sres.res;
 	 break;
       }
 
-      if (sres.isError && sres.val != VKI_EEXIST)
+      if (sres.isError && sres.err != VKI_EEXIST)
 	 return;		/* can't create file */
    }
 
