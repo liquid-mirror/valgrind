e84420bbf238a485155d681cddb40f886c7cb3f8
sewardj <sewardj@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2008-02-26 19:53:11 +0000

    Fix robustness problem in varinfo3.
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/DATASYMS@7485 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- memcheck/tests/varinfo3.c
+++ memcheck/tests/varinfo3.c
@@ -1,23 +1,23 @@
 
 /* Check for correct handling of static vs non-static, local vs
    non-local variables in a zero-biased executable. */
-
 /* Relevant compile flags are:
 
    -Wall -g -I$prefix/include/valgrind
 
    eg -Wall -g -I`pwd`/Inst/include/valgrind
 */
-
+/* Unfortunately 2008 Feb 26, requires its own filter_varinfo3, since
+   nonstatic_local_{un}def are not handled properly and so end up with
+   compiler-dependent names, eg static_local_def.2919 and
+   static_local_undef.2921.  So filter off the .nnnn part. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 #include "memcheck/memcheck.h"
-
 /* Cause memcheck to complain about the address "a" and so to print
    its best guess as to what "a" actually is.  a must be
    addressible. */
-
 void croak ( void* aV )
 {
   char* a = (char*)aV;
