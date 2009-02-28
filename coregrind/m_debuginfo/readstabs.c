c9a20abd1ccdaa5be022794994c9fcda78e5002a
sewardj <sewardj@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2009-02-28 22:15:13 +0000

    Cosmetic changes (2 x layout, 1 x control of debug printing).
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/DARWIN@9296 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- coregrind/m_debuginfo/readstabs.c
+++ coregrind/m_debuginfo/readstabs.c
@@ -46,14 +46,16 @@
 #include "priv_readstabs.h"        /* self */
 
 /* --- !!! --- EXTERNAL HEADERS start --- !!! --- */
-#if !defined(VGO_darwin)
+#if defined(VGO_linux)
 #  include <a.out.h> /* stabs defns */
-#else
+#elif defined(VGO_darwin)
 #  include <mach-o/nlist.h>
 #  define n_other n_sect
 #  if VG_WORDSIZE == 8
 #     define nlist nlist_64
 #  endif
+#else
+#  error "Unknown OS"
 #endif
 /* --- !!! --- EXTERNAL HEADERS end --- !!! --- */
 
@@ -137,11 +139,12 @@ void ML_(read_debuginfo_stabs) ( DebugInfo* di,
       const struct nlist *st = &stab[i];
       Char *string;
 
-      if (debug && 1) {
-         VG_(printf) ( "%2d  type=%d   othr=%d   desc=%d   value=0x%x   strx=%d  %s\n", i,
+      if (di->trace_symtab) {
+         VG_(printf) ( "%2d  type=%d   othr=%d   desc=%d   "
+                       "value=0x%x   strx=%d  %s\n", i,
                        st->n_type, st->n_other, st->n_desc, 
-                       (int)st->n_value,
-                       (int)st->n_un.n_strx, 
+                       (Int)st->n_value,
+                       (Int)st->n_un.n_strx, 
                        stabstr + st->n_un.n_strx );
       }
 
