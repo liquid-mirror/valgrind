91daa6c9a59ec53e65aceebc91368ed00fa08a8b
sewardj <sewardj@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2009-03-12 01:03:27 +0000

    Test fails on Darwin because pulling an allegedly invalid address out of a hat is a really stupid idea.  Fix it to generate an always-invalid place by first mmaping and then munmapping a page.
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/DARWIN@9370 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- memcheck/tests/signal2.c
+++ memcheck/tests/signal2.c
@@ -3,18 +3,38 @@
 #include <stdlib.h>
 #include <signal.h>
 
-void sig_hdlr ( int signo )
-{
-   printf ( "caught sig segv\n" );
-   exit(1);
+void sig_hdlr ( int signo ) {
+   printf ( "caught sig segv\n" ); exit(1);
 }
+char* get_bad_place ( void );
 
-int main ( void )
-{
+int main ( void ) {
+   char* badplace;
    printf ( "installing sig handler\n" );
    signal(SIGSEGV, sig_hdlr);
    printf ( "doing bad thing\n" );
-   * (int*) 65536 = 0;
+   badplace = get_bad_place();
+   *(int*)badplace = 0;
    printf ( "exited normally ?!\n" );
    return 0;
+}
+
+#include <sys/mman.h>
+#include <assert.h>
+#include <unistd.h>
+
+/* map a page, then unmap it, then return that address.  That
+   guarantees to give an address which will fault when accessed,
+   without making any assumptions about the layout of the address
+   space. */
+
+char* get_bad_place ( void )
+{
+   long pagesz = sysconf(_SC_PAGE_SIZE);
+   assert(pagesz == 4096 || pagesz == 65536);
+   void* ptr = mmap(0, pagesz, PROT_READ, MAP_ANON|MAP_PRIVATE, -1, 0);
+   assert(ptr != (void*)-1);
+   int r = munmap(ptr, 4096);
+   assert(r == 0);
+   return ptr;
 }
