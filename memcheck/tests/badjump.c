03ab9275d1d4d86ba66fd84515124dff070fbb26
sewardj <sewardj@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2009-03-23 08:36:33 +0000

    Generate a proper invalid page to jump to, rather than assuming 0xE000000 is invalid.
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/DARWIN@9491 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- memcheck/tests/badjump.c
+++ memcheck/tests/badjump.c
@@ -1,18 +1,38 @@
-
+char* get_bad_place ( void );
 int main ( void )
 {
 #if defined(__powerpc64__) || defined(_AIX)
    /* on ppc64-linux, a function pointer points to a function
       descriptor, not to the function's entry point.  Hence to get
-      uniform behaviour on all supported targets - a jump to 0xE000000
-      - the following is needed. */
+      uniform behaviour on all supported targets - a jump to an
+      unmapped page - the following is needed. */
    unsigned long long int fake_fndescr[3];
-   fake_fndescr[0] = 0xE000000;
+   fake_fndescr[0] = (unsigned long long int)get_bad_place();
    fake_fndescr[1] = 0;
    fake_fndescr[2] = 0;
    return ((int(*)(void)) fake_fndescr) ();
 #else
-   char* p = (char*)0xE000000;
+   char* p = get_bad_place();
    return ((int(*)(void)) p) ();
 #endif
+}
+
+#include <sys/mman.h>
+#include <assert.h>
+#include <unistd.h>
+
+/* map a page, then unmap it, then return that address.  That
+   guarantees to give an address which will fault when accessed,
+   without making any assumptions about the layout of the address
+   space. */
+
+char* get_bad_place ( void )
+{
+   long pagesz = sysconf(_SC_PAGE_SIZE);
+   assert(pagesz == 4096 || pagesz == 65536);
+   void* ptr = mmap(0, pagesz, PROT_READ, MAP_ANON|MAP_PRIVATE, -1, 0);
+   assert(ptr != (void*)-1);
+   int r = munmap(ptr, pagesz);
+   assert(r == 0);
+   return ptr;
 }
