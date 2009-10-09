8f0b62cf205d051777ca181c199f234e66b1f27d
sewardj <sewardj@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2009-10-09 00:04:51 +0000

    Add tests for PMOVMSKB for x86 and amd64.
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/ICC111@10899 a5019735-40e9-0310-863c-91ae7b9d1cf9

---
+++ memcheck/tests/amd64/pmovmskb64.c
@@ -0,0 +1,96 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "tests/malloc.h"
+#include <string.h>
+
+typedef  unsigned char  V128[16];
+typedef  unsigned char  V64[8];
+typedef  unsigned int   UInt;
+typedef  signed int     Int;
+typedef  unsigned char  UChar;
+typedef  unsigned long long int  ULong;
+
+static UChar randUChar ( void )
+{
+   static UInt seed = 80021;
+   seed = 1103515245 * seed + 12345;
+   return (seed >> 17) & 0xFF;
+}
+
+static void randV128 ( V128* v )
+{
+   Int i;
+   for (i = 0; i < 16; i++)
+      (*v)[i] = randUChar();
+}
+
+static void randV64 ( V64* v )
+{
+   Int i;
+   for (i = 0; i < 8; i++)
+      (*v)[i] = randUChar();
+}
+
+static void showV128 ( V128* v )
+{
+   Int i;
+   for (i = 0; i < 16; i++)
+      printf("%02x", (Int)(*v)[i]);
+}
+
+static void showV64 ( V64* v )
+{
+   Int i;
+   for (i = 0; i < 8; i++)
+      printf("%02x", (Int)(*v)[i]);
+}
+
+ULong do_pmovmskb_xmm_r64 ( V128* arg )
+{
+   ULong res;
+   __asm__ __volatile__(
+      "movupd  (%1), %%xmm11"     "\n\t"
+      "pmovmskb %%xmm11, %%r15"   "\n\t"
+      "movq %%r15, %0"            "\n"
+      : /*out*/ "=r"(res) : /*in*/ "r"(arg)
+                          : /*trash*/"memory","xmm1","r15"
+   );
+   return res;
+}
+
+ULong do_pmovmskb_mmx_r64 ( V64* arg )
+{
+   ULong res;
+   __asm__ __volatile__(
+      "movq  (%1), %%mm1"     "\n\t"
+      "pmovmskb %%mm1, %0"      "\n"
+      : /*out*/ "=r"(res) : /*in*/ "r"(arg)
+                          : /*trash*/"memory","mm1"
+   );
+   return res;
+}
+
+int main ( void )
+{
+   V128 argL;
+   V64 argS;
+   UInt res, i;
+   UChar* undef = malloc(1);
+   for (i = 0; i < 10; i++) {
+     randV128( &argL );
+     //if (i == 9) argL[5] = *undef;
+     res = do_pmovmskb_xmm_r64( &argL );
+     showV128( &argL );
+     printf("  %08x\n", res );
+   }
+   for (i = 0; i < 10; i++) {
+     randV64( &argS );
+     res = do_pmovmskb_mmx_r64( &argS );
+     showV64( &argS );
+     printf("  %08x\n", res );
+   }
+   free(undef);
+   return 0;
+}
