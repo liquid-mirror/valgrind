f6fa7866cb2cbb1be224ff3621824db96745e32f
sewardj <sewardj@a5019735-40e9-0310-863c-91ae7b9d1cf9>
2010-06-02 11:58:27 +0000

    An infrastructural change, intended to improve 64-bit support on MacOSX and hence improve support on MacOSX 10.6, which is 64-bit by default:
    Link the tool executables statically on MacOSX, as they are on Linux.
    This sidesteps a couple of problems:
    
    * no need to play address space games by mmaping huge blocks
      at startup.  This gets rid of the ~4 second startup delay
      for all programs in 64-bit mode on MacOSX.
    
    * only one copy of dyld exists in the process space, instead
      of two having to coexist
    
    Changes in the patch:
    
    * various tweaking of -fno-PIC etc for Darwin, so as to ensure
      that the tool executable code isn't PIC and so can be linked
      statically
    
    * Fix a conceptual hole which (afaik) has never been noticed
      before: the configure.in must specify the alternative load
      address (valt_) for the primary and secondary platforms
      separately.  This has only not been noticed till now because
      the alt load address has been the same on all Linux platforms
      (0x38000000).
    
      FTR, for 32-bit Darwin, I'm using 0x38000000 (as on Linux ..
      well, it works, so might as well) and for 64-bit, 
      0x138000000 (the x86 address + 4G), so as to avoid pagezero.
    
    * add a bit more debug printing to m_ume/macho.c.  Also take
      care not to honour client pagezero map requests either in
      32- or 64-bit mode.
    
    * get rid of dyld_stub_binding_helper stubs, whatever they are,
      in coregrind/m_start-{x86,amd64}-darwin.S.
    
    * get rid of the munmap of address-space-filling segments in
      m_main.c.
    
    * m_main: add own implementation of __udivdi3 and __umoddi3.
      Need to move this somewhere sane.
    
    * link_tool_exe.c: change darwin case handling to do a simple
      static link, and mostly ignore the invokation command line
      we're given.
    
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/branches/MACOSX106@11144 a5019735-40e9-0310-863c-91ae7b9d1cf9

--- coregrind/link_tool_exe.c
+++ coregrind/link_tool_exe.c
@@ -105,8 +105,52 @@ int main ( int argc, char** argv )
 
 #elif defined(VGO_darwin)
 
-/* Run the specified command as-is; ignore the specified load address
-   (argv[1]). */
+/* Plan is: look at the specified gcc invokation.  Ignore all parts of
+   it except the *.a, *.o and -o outfile parts.  Wrap them up in a new
+   command which looks (eg) as follows:
+
+   (64-bit):
+
+   /usr/bin/ld -static -arch x86_64 -macosx_version_min 10.5 \
+      -o memcheck-amd64-darwin -u __start -e __start \
+      -image_base 0x138000000 -stack_addr 0x13c000000 \
+      -stack_size 0x800000 \
+      memcheck_amd*.o \
+      ../coregrind/libcoregrind-amd64-darwin.a \
+      ../VEX/libvex-amd64-darwin.a
+
+   (32-bit)
+
+   /usr/bin/ld -static -arch i386 -macosx_version_min 10.5 \
+      -o memcheck-x86-darwin -u __start -e __start \
+      -image_base 0x38000000 -stack_addr 0x3c000000 \
+      -stack_size 0x800000 \
+      memcheck_x86*.o \
+      ../coregrind/libcoregrind-x86-darwin.a \
+      ../VEX/libvex-x86-darwin.a
+
+   The addresses shown above will actually work, although "for real" we
+   of course need to take it from argv[1].  In these examples the stack
+   is placed 64M after the executable start.  It is probably safer to
+   place it 64M before the executable's start point, so the executable
+   + data + bss can grow arbitrarily in future without colliding with
+   the stack.
+
+   There's one more twist: this executable (for the program in this
+   file) could be compiled as either 32- or 64-bit.  That has no
+   bearing at all on the word size of the executable for which we are
+   linking.  We need to know the latter since we need to hand to the
+   linker, "-arch x86_64" or "-arch i386".  Fortunately we can figure
+   this out by scanning the gcc invokation, which itself must contain
+   either "-arch x86_64" or "-arch i386".
+*/
+
+/* user configurable constants: how far before the exe should we
+   place the stack? */
+#define TX_STACK_OFFSET_BEFORE_TEXT (64 * 1024 * 1024)
+/* and how big should the stack be */
+#define TX_STACK_SIZE (8 * 1024 * 1024)
+
 
 // Don't NDEBUG this; the asserts are necesary for
 // safety checks.
@@ -115,44 +159,113 @@ int main ( int argc, char** argv )
 #include <stdio.h>
 #include <stdlib.h>
 
+static void add_to ( char** str, size_t* str_sz, char* to_add )
+{
+   size_t needed = strlen(to_add) +2/*paranoia*/;
+   size_t currlen = strlen(*str);
+   assert(currlen < *str_sz);
+
+   while (needed >= *str_sz - currlen) {
+      *str = realloc(*str, 2 * *str_sz);
+      assert(*str);
+      (*str_sz) *= 2;
+   }
+
+   assert(currlen < *str_sz);
+   assert(needed < *str_sz - currlen);
+   strcat(*str, to_add);
+}
+
+static int/*bool*/ is_dota_or_doto ( char* str )
+{
+   assert(str);
+   size_t n = strlen(str);
+   if (n < 2) return 0;
+   if (str[n-2] == '.' && (str[n-1] == 'a' || str[n-1] == 'o'))
+       return 1;
+   return 0;
+}
+
+/* Run the specified command as-is; ignore the specified load address
+   (argv[1]). */
+
 int main ( int argc, char** argv )
 {
-   int    i;
-   size_t reqd = 0;
+   int i;
 
    // expect at least: alt-load-address gcc -o foo bar.o
    assert(argc > 5);
 
-   // check for plausible-ish alt load address
-   char* ala = argv[1];
-   assert(ala[0] == '0');
-   assert(ala[1] == 'x');
+   // check for plausible-ish alt load address, and get hold
+   // of it
+   char* ala_str = argv[1];
+   unsigned long long int ala = 0;
 
-   // command to run is argv[2 ..]
+   assert(ala_str[0] == '0');
+   assert(ala_str[1] == 'x');
 
-   // so, build up the complete command here:
-   // argv[2 ..]
+   int r = sscanf(ala_str, "0x%llx", &ala);
+   assert(r == 1);
 
-   // first, do length safety checks
-   for (i = 2; i < argc; i++)
-      reqd += 1+ strlen(argv[i]);
+   // get hold of the outfile name
+   char* outfile_name = NULL;
+   for (i = 1; i < argc-1; i++) {
+      if (0 == strcmp(argv[i], "-o")) {
+         outfile_name = argv[i+1];
+         break;
+      }
+   }
+   assert(outfile_name);
 
-   reqd += 1;
-   char* cmd = calloc(reqd,1);
+   // get hold of the string following -arch
+   char* arch_str = NULL;
+   for (i = 1; i < argc-1; i++) {
+      if (0 == strcmp(argv[i], "-arch")) {
+         arch_str = argv[i+1];
+         break;
+      }
+   }
+   assert(arch_str);
+
+   // build the command line
+   size_t cmd_sz = 1;
+   char*  cmd    = calloc(cmd_sz, 1);
    assert(cmd);
 
-   for (i = 2; i < argc; i++) {
-     strcat(cmd, " ");
-     strcat(cmd, argv[i]);
+   add_to(&cmd, &cmd_sz,  "/usr/bin/ld");
+   add_to(&cmd, &cmd_sz,  " -static");
+   add_to(&cmd, &cmd_sz,  " -arch ");
+   add_to(&cmd, &cmd_sz,  arch_str);
+   add_to(&cmd, &cmd_sz,  " -macosx_version_min 10.5");
+   add_to(&cmd, &cmd_sz,  " -o ");
+   add_to(&cmd, &cmd_sz,  outfile_name);
+   add_to(&cmd, &cmd_sz,  " -u __start -e __start");
+
+   char buf[40];
+   sprintf(buf, "0x%llx", ala);
+   add_to(&cmd, &cmd_sz,  " -image_base ");
+   add_to(&cmd, &cmd_sz,  buf);
+
+   sprintf(buf, "0x%llx", ala - TX_STACK_OFFSET_BEFORE_TEXT);
+   add_to(&cmd, &cmd_sz,  " -stack_addr ");
+   add_to(&cmd, &cmd_sz,  buf);
+
+   sprintf(buf, "0x%llx", (unsigned long long int)TX_STACK_SIZE);
+   add_to(&cmd, &cmd_sz,  " -stack_size ");
+   add_to(&cmd, &cmd_sz,  buf);
+
+   for (i = 3; i < argc; i++) {
+      if (is_dota_or_doto(argv[i])) {
+         add_to(&cmd, &cmd_sz,  " ");
+         add_to(&cmd, &cmd_sz,  argv[i]);
+      }
    }
 
-   assert(cmd[reqd-1] == 0);
-
-   if (0) printf("\n");
+   if (1) printf("\n");
    printf("link_tool_exe: %s\n", cmd);
-   if (0) printf("\n");
+   if (1) printf("\n");
 
-   int r = system(cmd);
+   r = system(cmd);
 
    free(cmd);
 
